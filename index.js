3141580d1b5f8a288e01cf85156a6bb33e1d2ef5
pacifist <phongpak2925809@gmail.com>
2019-04-22 13:21:53 +0700

    route

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/allrooms', (req, res) => res.send('Hello World!'))
+app.post('/allrooms', (req, res) => res.send('Hello World!'))
+app.put('/allrooms', (req, res) => res.send('Hello World!'))
+app.delete('/allrooms', (req, res) => res.send('Hello World!'))
+
+app.get('/room', (req, res) => res.send('Hello World!'))
+app.post('/rooms', (req, res) => res.send('Hello World!'))
+app.put('/room', (req, res) => res.send('Hello World!'))
+app.delete('/room', (req, res) => res.send('Hello World!'))
+
+app.get('/users', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
